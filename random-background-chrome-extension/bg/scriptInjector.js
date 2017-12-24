f67d613d27ad06d2edf77bd778bc663a690723c5
abucraft <499817366@qq.com>
2017-12-24 23:03:51 +0800

    Code cannot be inserted in top-level about:-frames.

--- random-background-chrome-extension/bg/scriptInjector.js
+++ random-background-chrome-extension/bg/scriptInjector.js
@@ -1,34 +1,16 @@
-var googleHomeReg = /https?:\/\/www\.google\.\w{2,3}(\.\w{1,3})?\//
-var searchingReg = /(o?q=)|(\/doodles)|(patents)/;
-var settingStrings = {
-    empty_new_tab: 'empty new tab',
-    empty_new_tab_zh: '空白新标签页',
-}
-
-chrome.contextMenus.create({
-    id: "emptyNewTab", title: _getString(settingStrings, "empty_new_tab"),
-    type: "checkbox", contexts: ["browser_action"], checked: rootSetting.settings.emptyNewTab,
-    onclick: function (info) {
-        rootSetting.settings.emptyNewTab=info.checked;
-        _setLocalStorage("settings", rootSetting.settings);
-    }
-});
-
 chrome.tabs.onCreated.addListener(function (tab) {
     if (tab.url == "chrome://newtab/") {
-        var redirection = rootSetting.settings.emptyNewTab?"newtab.html":"https://www.google.com/_/chrome/newtab";
         chrome.tabs.update(tab.id, {
-            url: redirection
+            url: "about:blank"
         })
     }
 });
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    if (!tab.url.match(googleHomeReg) || tab.url.match(searchingReg)) { return; } // Not google
+    if (!tab.url === "about:blank") { return; } // Not blank
     if (changeInfo.status !== 'complete') { return; }
     console.log(rootSetting.settings);
-    if (!rootSetting.settings.applyOnGoogle) { return; }
-    chrome.tabs.sendMessage(tabId, {type:"verify_injected"}, function (response) {
+    chrome.tabs.sendMessage(tabId, { type: "verify_injected" }, function (response) {
         if (!response) {
             var scripts = [
                 "lib/jquery-3.2.0.min.js",
@@ -49,14 +31,14 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
                 p = p.then(function () {
                     return new Promise(function (res, rej) {
                         //console.log(`load: ${file}`)
-                        chrome.tabs.executeScript(tabId, { file: file }, function () {
+                        chrome.tabs.executeScript(tabId, { file: file, matchAboutBlank: true }, function () {
                             res();
                         });
                     })
                 })
             });
             csses.forEach(function (file) {
-                chrome.tabs.insertCSS(tabId, { file: file });
+                chrome.tabs.insertCSS(tabId, { file: file, matchAboutBlank: true });
             })
         }
     })
